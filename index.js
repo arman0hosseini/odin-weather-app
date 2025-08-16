75ada7116af5a3575408b0411af7debebbc8d598
Arman Hosseini <arman0hosseini@gmail.com>
2025-08-16 22:19:32 +0330

    meow

---
+++ index.js
@@ -0,0 +1,11 @@
+// function initializeUIHandlers() {
+//     async function weatherFetch(url, userLocation) {
+//         const location = encodeURIComponent(userLocation);
+//         const myUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/last4days?unitGroup=metric&key=FVVWCUV5PCV8XMCJ63U33VDMT`
+//         const jsonResponse = await (await fetch(myUrl)).json();
+
+//     }
+
+
+// }
+
